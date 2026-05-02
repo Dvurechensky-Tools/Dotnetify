2b826fe09b0d2af1cbde66aefba224f7e786996b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-02 19:37:06 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-02

--- app/Dotnetify/Processors/DotnetifyContext.cs
+++ app/Dotnetify/Processors/DotnetifyContext.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 мая 2026 07:14:30
- * Version: 1.0.19
+ * Last Updated: 02 мая 2026 19:37:02
+ * Version: 1.0.20
  */
 
 using NSwag;
