490badc6fb0e93cb431003482edb4c0bc2ffff5b
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-05-07 15:47:16 +0300

    Auto-update headers via pre_commit.ps1 - 2026-05-07

--- app/Dotnetify/Processors/PackageResolve/Core/DotnetBuildResult.cs
+++ app/Dotnetify/Processors/PackageResolve/Core/DotnetBuildResult.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 06 мая 2026 10:51:17
- * Version: 1.0.24
+ * Last Updated: 07 мая 2026 15:47:13
+ * Version: 1.0.25
  */
 
 using System.Diagnostics;
