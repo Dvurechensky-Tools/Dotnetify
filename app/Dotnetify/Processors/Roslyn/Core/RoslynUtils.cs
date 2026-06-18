2eb689643ccf9e5c93d338e7cffb4ec6d95b3d8d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-18 07:13:27 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-18

--- app/Dotnetify/Processors/Roslyn/Core/RoslynUtils.cs
+++ app/Dotnetify/Processors/Roslyn/Core/RoslynUtils.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 июня 2026 07:11:38
- * Version: 1.0.65
+ * Last Updated: 18 июня 2026 07:13:23
+ * Version: 1.0.66
  */
 
 using Microsoft.CodeAnalysis;
