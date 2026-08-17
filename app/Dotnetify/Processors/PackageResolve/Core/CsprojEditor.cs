f4b7602a6b173a7c1888c762a038721052d9def4
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-17 07:12:52 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-17

--- app/Dotnetify/Processors/PackageResolve/Core/CsprojEditor.cs
+++ app/Dotnetify/Processors/PackageResolve/Core/CsprojEditor.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 16 августа 2026 14:47:24
- * Version: 1.0.124
+ * Last Updated: 17 августа 2026 07:12:49
+ * Version: 1.0.125
  */
 
 using System.Xml.Linq;
