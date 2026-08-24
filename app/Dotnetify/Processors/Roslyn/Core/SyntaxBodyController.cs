f3bace2b6206fc3704bd983e9a8ba06c24469442
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-08-24 07:15:16 +0300

    Auto-update headers via pre_commit.ps1 - 2026-08-24

--- app/Dotnetify/Processors/Roslyn/Core/SyntaxBodyController.cs
+++ app/Dotnetify/Processors/Roslyn/Core/SyntaxBodyController.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 августа 2026 07:15:32
- * Version: 1.0.131
+ * Last Updated: 24 августа 2026 07:15:13
+ * Version: 1.0.132
  */
 
 using Microsoft.CodeAnalysis.CSharp;
