cb2fc0552c7b6ea4012e33e872d483c24044aba7
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-11 06:58:12 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-11

--- app/Dotnetify/Processors/PackageResolve/Core/PackageResolverOrchestrator.cs
+++ app/Dotnetify/Processors/PackageResolve/Core/PackageResolverOrchestrator.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 сентября 2026 10:13:22
- * Version: 1.0.148
+ * Last Updated: 11 сентября 2026 06:58:09
+ * Version: 1.0.149
  */
 
 using System.Text;
