b2e58c3dba36afceb1d96a2ec195148fc9620a53
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-13 06:57:50 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-13

--- app/Dotnetify/Processors/PackageResolve/Core/BuildErrorParser.cs
+++ app/Dotnetify/Processors/PackageResolve/Core/BuildErrorParser.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 12 сентября 2026 06:58:14
- * Version: 1.0.150
+ * Last Updated: 13 сентября 2026 06:57:46
+ * Version: 1.0.151
  */
 
 using System.Text.RegularExpressions;
